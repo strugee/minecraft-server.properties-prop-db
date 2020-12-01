7c92eeb86fdd199cbca8013bfc01226700afb7e4
AJ Jordan <alex@strugee.net>
2020-12-01 06:58:10 -0500

    Follow redirects in the update helper script

--- generate-default-server.properties.sh
+++ generate-default-server.properties.sh
@@ -3,11 +3,11 @@
 set -eu
 
 echo Retrieving server download URL.
-URL=$(curl https://www.minecraft.net/en-us/download/server/ | grep launcher.mojang.com | cut -d'"' -f2)
+URL=$(curl -L https://www.minecraft.net/en-us/download/server/ | grep launcher.mojang.com | cut -d'"' -f2)
 echo Using $URL.
 
 echo Downloading server.
-curl $URL > server.jar
+curl -L $URL > server.jar
 
 echo Running server.
 test -f server.properties && rm server.properties
